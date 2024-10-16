10407eeb765f3e61fa2e65cfa0a6484425a4b8b3
A-L-O-K <abhijitramesh2k@gmail.com>
2024-10-16 10:42:45 +0530

    git trial 3

--- GITTRAIL/model.py
+++ GITTRAIL/model.py
@@ -5,6 +5,7 @@ class Model(torch.nn.Module):
         super(Model, self).__init__()
         self.fc = torch.nn.Linear(1, 1)
         self.fc.weight.data.fill_(1.0)
+        self.fc.bias.data.fill_(0.0)
         
     def forward(self, x):
         return self.fc(x)
